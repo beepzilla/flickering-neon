480a64dd455478ce77351235cf363c7fd91b0793
Josh <beepzilla@gmail.com>
2024-03-26 06:08:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let isPlaying = false;
 let currentSongIndex = 0;
 const songs = [
     'https://ipfs.io/ipfs/Qma4cKFvWvfqoKXpB3KCPhFsz2ySje5PSNywTBZff8eXT4/stardust-danijel-zambo-main-version-03-13-1372.mp3',
-    'https://ipfs.io/ipfs/Qma4cKFvWvfqoKXpB3KCPhFsz2ySje5PSNywTBZff8eXT4/stellar-escape-prigida-main-version-25318-03-02.mp3',
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/pencil-crayons-sulyya-main-version-27127-03-10.mp3',
     'https://ipfs.io/ipfs/Qma4cKFvWvfqoKXpB3KCPhFsz2ySje5PSNywTBZff8eXT4/stellar-escape-prigida-main-version-25318-03-02%20(1).mp3',
-    'song4.mp3',
-    'song5.mp3',
-    'song6.mp3',
-    'song7.mp3',
-    'song8.mp3'
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/game-over-danijel-zambo-main-version-02-03-1394.mp3',
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/sunset-in-junipero-bach-main-version-19643-03-22.mp3',
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/easy-arcade-hartzmann-main-version-28392-02-32.mp3',
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/milky-way-mountaineer-main-version-28369-02-20.mp3',
+    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/space-ranger-moire-main-version-03-04-10814.mp3'
 ];
 
 playBtn.addEventListener('click', () => {
