796d6894c901ef7d186386545b4a966a9b552e4b
Josh <beepzilla@gmail.com>
2024-03-31 23:43:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,5 @@
 const textElement = document.getElementById('text');
 const imageContainer = document.getElementById('image-container');
-const audioPlayer = document.getElementById('audio-player');
-const playBtn = document.getElementById('play-btn');
-const pauseBtn = document.getElementById('pause-btn');
-const nextBtn = document.getElementById('next-btn');
-const audioSource = document.getElementById('audio-source');
-
-let isPlaying = false;
-let currentSongIndex = 0;
-const songs = [
-    'https://ipfs.io/ipfs/Qma4cKFvWvfqoKXpB3KCPhFsz2ySje5PSNywTBZff8eXT4/stellar-escape-prigida-main-version-25318-03-02%20(1).mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/game-over-danijel-zambo-main-version-02-03-1394.mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/sunset-in-junipero-bach-main-version-19643-03-22.mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/easy-arcade-hartzmann-main-version-28392-02-32.mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/milky-way-mountaineer-main-version-28369-02-20.mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/space-ranger-moire-main-version-03-04-10814.mp3',
-    'https://ipfs.io/ipfs/Qma4cKFvWvfqoKXpB3KCPhFsz2ySje5PSNywTBZff8eXT4/stardust-danijel-zambo-main-version-03-13-1372.mp3',
-    'https://ipfs.io/ipfs/QmdcYArSqshxQ5LE6BgWgQQPnz7AWB4AFHXwsbBusqQXfk/pencil-crayons-sulyya-main-version-27127-03-10.mp3'
-];
-
-playBtn.addEventListener('click', () => {
-    if (!isPlaying) {
-        audioPlayer.play();
-    } else {
-        audioPlayer.pause();
-    }
-});
-
-nextBtn.addEventListener('click', () => {
-    currentSongIndex = (currentSongIndex + 1) % songs.length;
-    audioSource.src = songs[currentSongIndex];
-    audioPlayer.load();
-    audioPlayer.play();
-});
-
-audioPlayer.addEventListener('play', () => {
-    isPlaying = true;
-    playBtn.style.display = 'none';
-    pauseBtn.style.display = 'inline';
-});
-
-audioPlayer.addEventListener('pause', () => {
-    isPlaying = false;
-    playBtn.style.display = 'inline';
-    pauseBtn.style.display = 'none';
-});
-
-audioPlayer.addEventListener('ended', () => {
-    nextBtn.click();
-});
-
 
 document.addEventListener('mousemove', (event) => {
     applyGlowEffect(event, textElement, 'text');
